36b11d4d4eaa37822110f82bd20e5f3c2b8bab34
Gray Morrow <tgrahammor@gmail.com>
2020-12-04 13:50:03 -0700

    Boot project created

---
+++ OrganMatcherREST/src/main/java/com/skilldistillery/organmatcher/OrganMatcherRestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.organmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganMatcherRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganMatcherRestApplication.class, args);
+	}
+
+}
---
+++ OrganMatcherREST/src/test/java/com/skilldistillery/organmatcher/OrganMatcherRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.organmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganMatcherRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
