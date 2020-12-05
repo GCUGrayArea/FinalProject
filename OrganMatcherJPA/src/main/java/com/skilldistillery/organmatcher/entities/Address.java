7a094a3cc979840b70fd74e13576e2a72c6fa761
Gray Morrow <tgrahammor@gmail.com>
2020-12-05 10:13:13 -0700

    Address now meets bean requirements

--- OrganMatcherJPA/src/main/java/com/skilldistillery/organmatcher/entities/Address.java
+++ OrganMatcherJPA/src/main/java/com/skilldistillery/organmatcher/entities/Address.java
@@ -15,5 +15,73 @@ public class Address {
 	private String city;
 	private String state;
 	private String zip;
+	
+	public Address() {
+		super();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet1() {
+		return street1;
+	}
+	public void setStreet1(String street1) {
+		this.street1 = street1;
+	}
+	public String getStreet2() {
+		return street2;
+	}
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street1=" + street1 + ", street2=" + street2 + ", city=" + city + ", state="
+				+ state + ", zip=" + zip + "]";
+	}
 
 }
