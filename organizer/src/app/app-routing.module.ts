30a0e7372b4f2cc6bb4153e28ff49516d5812353
kjedmeade <kjedmeade@gmail.com>
2020-12-07 16:12:55 -0700

    additions to transplant request

--- organizer/src/app/app-routing.module.ts
+++ organizer/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
+import { TransplantRequestListComponent } from './components/transplant-request-list/transplant-request-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaitentListComponent } from './components/paitent-list/paitent-list.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'patientList'},
-  { path: 'patientList', component: PaitentListComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'patient' },
+  { path: 'transplantRequest', component: TransplantRequestListComponent },
+  { path: 'patient', component: PaitentListComponent },
+  // { path: '**', component: ErrorComponent }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
