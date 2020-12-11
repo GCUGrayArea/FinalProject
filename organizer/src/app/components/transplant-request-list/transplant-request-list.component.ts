64b589563ced885ed94e6c23e674a643c33947e8
Bronson Spies <spiesbronson@gmail.com>
2020-12-11 15:17:44 -0700

    small debugs

--- organizer/src/app/components/transplant-request-list/transplant-request-list.component.ts
+++ organizer/src/app/components/transplant-request-list/transplant-request-list.component.ts
@@ -204,6 +204,7 @@ console.log(err);
     }
     // this.id = null;
     this.filtered = true;
+    this.selectedType =null;
 
   }
   displayTable(): void {
@@ -245,7 +246,7 @@ findByDonorIsNull(){
 console.log(err);
 
     });
-
+this.filtered = true;
 
 }
 open(content, tRequest) {
